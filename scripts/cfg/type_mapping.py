74309f085e97f2b567e7f878954b18ec31e5287d
leo_moraes <developerleo.it@gmail.com>
2025-05-21 01:30:57 +0100

    data and model

---
+++ scripts/cfg/type_mapping.py
@@ -0,0 +1,17 @@
+
+
+
+## == TYPES OF COLUMNS
+type_mapping = {
+    'code': 'object',       
+    'codein': 'object',   
+    'name': 'object',       
+    'high': 'float64',
+    'low': 'float64',
+    'varBid': 'float64',     
+    'pctChange': 'float64',
+    'bid': 'float64',
+    'ask': 'float64',
+    'timestamp': 'datetime64[ns]',
+    'create_date': 'datetime64[ns]'
+}
---
+++ scripts/dataset_utils/utiils.py
@@ -0,0 +1,63 @@
+# == ================================================ == #
+# == Get data from Awesome API                        == #
+# == GSHEETS: https://docs.awesomeapi.com.br/         == #
+# == ================================================ == #
+from cfg.type_mapping import type_mapping
+import pandas as pd
+import requests
+import time
+
+
+
+
+def get_exchange(coin: str, awesome_api_key: str) -> list:
+
+    days_number = 360
+
+    headers = {
+    'Accept': 'application/json'
+    }
+
+    params = {
+        'token': awesome_api_key
+    }
+
+  
+    
+    ## == DO REQUEST TO API
+    url_endpoint = f"https://economia.awesomeapi.com.br/json/daily/{coin}/{days_number}"
+    response = requests.request("GET", url_endpoint, headers=headers, params=params)
+
+    print(f'Response req for coin {coin}: {response.status_code}')
+
+    response = response.json()
+
+    return response
+
+
+
+
+
+def adjuste_null_values(df: pd.DataFrame, resp: list) -> pd.DataFrame:
+
+    df['code'] = resp[0]['code']
+    df['codein'] = resp[0]['codein']
+    df['name'] = resp[0]['name']
+    df['create_date'] = resp[0]['create_date']
+
+    df['timestamp'] = df['timestamp'].apply(lambda epoch_time: time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(float(epoch_time))))
+
+    return df
+
+
+
+
+def adjust_types(df: pd.DataFrame) -> pd.DataFrame:
+
+    for col, dtype in type_mapping.items():
+        if dtype == 'datetime64[ns]':
+            df[col] = pd.to_datetime(df[col])
+        else:
+            df[col] = df[col].astype(dtype)
+
+    return df
